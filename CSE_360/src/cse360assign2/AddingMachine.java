e5f25c93b96bf6841e52a0e6d50db40250079e64
pbcin <pbcin@LAPTOP-2R4916PB>
2019-10-09 21:19:41 -0700

    Initial commit of project.

---
+++ CSE_360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,71 @@
+package cse360assign2;
+
+/**
+ * The AddingMachine class performs adding and subtracting
+ * operations on a total. A record of the operations is
+ * recorded and is returned by toString().
+ * 
+ * @author Connor Greig
+ */
+
+public class AddingMachine
+{
+	private int total;
+	
+	/**
+	 * Sets a new AddingMachine object's total value to 0.
+	 */
+	public AddingMachine ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Fetches the total value of the AddingMachine object.
+	 * 
+	 * @return the current value of total
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds the parameter value to the total.
+	 * 
+	 * @param value	 the value to be added to total
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	/**
+	 * Subtracts the parameter value from the total.
+	 * 
+	 * @param value  the value to be subtracted to total
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	/**
+	 * Returns a string that consists of previous add and subtract
+	 * operations so that they may be printed out.
+	 * 
+	 * @return a String representation of the AddingMachine's operations
+	 */
+	public String toString ()
+	{
+		return "";
+	}
+
+	/**
+	 * Clears the string of operations of this AddingMachine object.
+	 */
+	public void clear()
+	{
+	
+	}
+}
